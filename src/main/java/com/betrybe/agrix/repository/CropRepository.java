126a318abe68f9670bea8ec0dc74959f2945d6eb
mairess <trabalho.maires@gmail.com>
2024-06-03 15:52:48 -0300

    refactor: remove unncessary method

--- src/main/java/com/betrybe/agrix/repository/CropRepository.java
+++ src/main/java/com/betrybe/agrix/repository/CropRepository.java
@@ -1,7 +1,6 @@
 package com.betrybe.agrix.repository;
 
 import com.betrybe.agrix.entity.Crop;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +10,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CropRepository extends JpaRepository<Crop, Long> {
 
-  List<Crop> findByFarmId(Long farmId);
 }
